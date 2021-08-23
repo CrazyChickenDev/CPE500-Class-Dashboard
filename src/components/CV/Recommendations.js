03f7e0dd3fc4be73208b9d7cef91e8c38d438bd0
0x00CCSec <Dannychukz15@gmail.com>
2021-08-23 06:22:32 +0100

    new uploads

--- src/components/CV/Recommendations.js
+++ src/components/CV/Recommendations.js
@@ -1,42 +1,15 @@
 import React from 'react'
-import { Card, Col, Divider, Row, Avatar } from 'antd'
+import { Card, Col, Row, Avatar } from 'antd'
 
 const Recommendations = ({ recommendations }) => (
   <>
     <h3 className="text-center mt-1 mb-0">Course Representatives and Lecturers</h3>
     <Card>
       {recommendations.map((rec, i) => (
-        <Row type="flex" justify="space-around" key={i} className="text-lg">
-          <Col span={5}>
+        <Row type="flex" justify="center" key={i} className="text-lg">
+          <Col span={20}>
             <h3 align = "center" className="mb-0">{rec.name}</h3>
-            <p>{rec.jobTitle}</p>
-            <Avatar
-              icon={<img src={`/people/${rec.photo}`} />}
-              alt={rec.name}
-              size={50}
-            />
-          </Col>
-          <Col span={5}>
-            <h3 align = "center" className="mb-0">{rec.name}</h3>
-            <p>{rec.jobTitle}</p>
-            <Avatar
-              icon={<img src={`/people/${rec.photo}`} />}
-              alt={rec.name}
-              size={50}
-            />
-          </Col>
-          <Col span={5}>
-            <h3 align = "center" className="mb-0">{rec.name}</h3>
-            <p>{rec.jobTitle}</p>
-            <Avatar
-              icon={<img src={`/people/${rec.photo}`} />}
-              alt={rec.name}
-              size={50}
-            />
-          </Col>
-          <Col span={5}>
-            <h3 align = "center" className="mb-0">{rec.name}</h3>
-            <p>{rec.jobTitle}</p>
+            <p align = "center">{rec.jobTitle}</p>
             <Avatar
               icon={<img src={`/people/${rec.photo}`} />}
               alt={rec.name}

19d50f9bc37d88eb602e19a393e6d0ad9d01b4b7
0x00CCSec <Dannychukz15@gmail.com>
2021-08-23 06:33:04 +0100

    new uploads

--- src/components/CV/Recommendations.js
+++ src/components/CV/Recommendations.js
@@ -6,9 +6,9 @@ const Recommendations = ({ recommendations }) => (
     <h3 className="text-center mt-1 mb-0">Course Representatives and Lecturers</h3>
     <Card>
       {recommendations.map((rec, i) => (
-        <Row type="flex" justify="center" key={i} className="text-lg">
+        <Row type="flex" justify="space-around" key={i} className="text-lg">
           <Col span={5}>
-            <h3 align = "center" className="mb-0">{rec.name}</h3>
+            <h3 className="mb-0">{rec.name}</h3>
             <p align = "center">{rec.jobTitle}</p>
             <Avatar align = "center"
               icon={<img src={`/people/${rec.photo}`} />}
